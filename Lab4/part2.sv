0a3b2caad7f4adf43797217c9c109a0d022a0f32
Xiao215 <xiaozhang20030215@gmail.com>
2022-10-21 13:18:12 -0400

    updated and fixed

--- Lab4/part2.sv
+++ Lab4/part2.sv
@@ -1,23 +1,27 @@
-module part2(Clock, Reset b, Data, Function, ALUout);
-    input logic Clock, Reset;
+module part2(Clock, Reset_b, Data, Function, ALUout);
+    input logic Clock, Reset_b;
     input logic [3:0] Data;
-    input logic [3:0] Function;
+    input logic [2:0] Function;
     output logic [7:0] ALUout;
-    wire logic [3:0] B;
+     logic [7:0] result;
+     logic [3:0] B;
     always_comb
     begin
         case (Function)
-            4'b0000: ALUout = Data + B;
-            4'b0001: ALUout = Data * B;
-            4'b0010: ALUout = B<<Data;
-            default: ALUout=ALUout;
+            3'b000: result = Data + B;
+            3'b001: result = Data * B;
+            3'b010: result = B<<Data;
+            default: result=result;
         endcase
     end
     always_ff @(posedge Clock)
     begin
-        if (reset_b) 
-            B <= 4’b0000;
+        ALUout<= result;
+        if (Reset_b) 
+            B <= 4'b0000;
         else
-            B<=ALUOut[3:0];
+            B<=ALUout[3:0];
     end
 endmodule
+
+// when do u do <= and when = and when assign? are they happening same time or in order?
