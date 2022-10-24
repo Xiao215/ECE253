8c3b81d66ce1520f761123c52e04640af752256d
Xiao215 <xiaozhang20030215@gmail.com>
2022-10-24 15:01:02 -0400

    updated and fixed

--- Lab4/part3.sv
+++ Lab4/part3.sv
@@ -2,10 +2,11 @@ module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
     input logic [3:0] Data_IN;
     input logic clock, reset, ParallelLoadn, RotateRight, ASRight;
     output logic [3:0] Q;
-    part3a p1(.left(Data_IN[0]), .right(Data_IN[2]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[3]), .Q(Q[3]));
-    part3a p2(.left(Data_IN[3]), .right(Data_IN[1]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[2]),  .Q(Q[2]));
-    part3a p3(.left(Data_IN[2]), .right(Data_IN[0]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[1]), .Q(Q[1]));
-    part3a p4(.left(Data_IN[1]), .right(Data_IN[3]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[0]), .Q(Q[0]));
+    assign Q = Data_IN;
+    part3a p1(.left(Q[0]), .right(Q[2]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Q[3]), .Q(Q[3]));
+    part3a p2(.left(Q[3]), .right(Q[1]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Q[2]),  .Q(Q[2]));
+    part3a p3(.left(Q[2]), .right(Q[0]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Q[1]), .Q(Q[1]));
+    part3a p4(.left(Q[1]), .right(Q[3]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Q[0]), .Q(Q[0]));
     always_ff @(posedge clock)
     begin
         if(ASRight)
