68ba1871a58897f5f7c532abde01d4a1238a6295
Xiao215 <xiaozhang20030215@gmail.com>
2022-10-26 18:04:22 -0400

    updated and fixed

--- Lab4/part3.sv
+++ Lab4/part3.sv
@@ -1,53 +1,46 @@
-module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
+module part3(input logic clock, reset, ParallelLoadn,RotateRight, ASRight, input logic[3:0] Data_IN, output logic[3:0] Q);
+	logic [3:0] internal;
 	
-	input logic clock, reset, ParallelLoadn, RotateRight, ASRight;
-	input logic [3:0] Data_IN;
-	output logic [3:0] Q;
-	
-	logic [3:0]q;
-	
-	D_flip_flop u (clock, reset, q, Q);
+	D_flipflop d0 (clock, reset, internal, Q);
 	
 	always_comb
-		begin
-			if (ParallelLoadn==0)
-				begin
-				q = Data_IN;
-				end
-			else if (ParallelLoadn ==1 & RotateRight == 1 & ASRight==0)
-				begin
-				q[3] = Q[0];
-				q[2] = Q[3];
-				q[1] = Q[2];
-				q[0] = Q[1];
-				end
-			else if (ParallelLoadn == 1 & RotateRight == 1 & ASRight == 1)
-				begin
-				q[3] = Q[3];
-				q[2] = Q[3];
-				q[1] = Q[2];
-				q[0] = Q[1];
-				end
-			else if (ParallelLoadn == 1 & RotateRight == 0)
-				begin
-				q[3] = Q[2];
-				q[2] = Q[1];
-				q[1] = Q[0];
-				q[0] = Q[3];
-				end
-		end
-endmodule
-
-module D_flip_flop(
-	input logic clk ,
-	input logic reset_b ,
-	input logic [3:0] d,
-	output logic [3:0] q
-	);
-	always_ff @(posedge clk)
 	begin
-		if (reset_b) q <= 4'b0;
-		else q <= d;
+		if(ParallelLoadn) 
+		begin
+			if(!RotateRight) 
+			begin
+				internal[3] = Q[2];
+				internal[2] = Q[1];
+				internal[1] = Q[0];
+				internal[0] = Q[3];
+			end
+			else
+			begin
+				internal[2] = Q[3];
+				internal[1] = Q[2];
+				internal[0] = Q[1];
+			    if(ASRight)
+				begin
+				internal[3] = Q[3];
+				end
+				else
+				begin
+				internal[3] = Q[0];
+				end
+			end
+		end
+		else
+		begin
+		    internal = Data_IN;
+		end
 	end
-	
+endmodule
+				
+				
+module D_flipflop(input logic clock, reset_b, input logic [3:0] D, output logic [3:0] Q);
+	always_ff @(posedge clock)
+	begin
+		if (reset_b) Q <= 4'b0;
+		else Q <= D;
+	end
 endmodule
