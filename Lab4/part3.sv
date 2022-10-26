a3b4e61a65c2d18c798a6a849e63ab211378adae
Xiao215 <xiaozhang20030215@gmail.com>
2022-10-26 18:38:02 -0400

    updated and fixed

--- Lab4/part3.sv
+++ Lab4/part3.sv
@@ -1,46 +1,44 @@
-module part3(input logic clock, reset, ParallelLoadn,RotateRight, ASRight, input logic[3:0] Data_IN, output logic[3:0] Q);
-	logic [3:0] internal;
-	
-	D_flipflop d0 (clock, reset, internal, Q);
-	
-	always_comb
-	begin
-		if(ParallelLoadn) 
-		begin
-			if(!RotateRight) 
-			begin
-				internal[3] = Q[2];
-				internal[2] = Q[1];
-				internal[1] = Q[0];
-				internal[0] = Q[3];
-			end
-			else
-			begin
-				internal[2] = Q[3];
-				internal[1] = Q[2];
-				internal[0] = Q[1];
-			    if(ASRight)
-				begin
-				internal[3] = Q[3];
-				end
-				else
-				begin
-				internal[3] = Q[0];
-				end
-			end
-		end
-		else
-		begin
-		    internal = Data_IN;
-		end
-	end
+module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
+    input logic [3:0] Data_IN;
+    input logic clock, reset, ParallelLoadn, RotateRight, ASRight;
+    logic [3:0] result;
+    output logic [3:0] Q;
+    always_ff @(posedge clock)
+    begin
+        if(ParallelLoadn == 0) begin
+                Q <= Data_IN;
+            end else if (RotateRight == 0) begin
+                Q <= {Q[2:0], Q[3]};
+            end else if (RotateRight == 0) begin
+                Q<= {Q[2:0], Q[3]};
+            end else if (ASRight == 0) begin
+                Q<= {Q[0], Q[3:1]};
+            end else begin
+                Q<= {Q[3], Q[3:1]};
+        end
+    end
 endmodule
-				
-				
-module D_flipflop(input logic clock, reset_b, input logic [3:0] D, output logic [3:0] Q);
-	always_ff @(posedge clock)
-	begin
-		if (reset_b) Q <= 4'b0;
-		else Q <= D;
-	end
+
+
+module part3a(left, right, LoadLeft,D, loadn, clock, reset, Q);
+    input logic left, right, LoadLeft, loadn, clock, reset,D;
+    output logic Q;
+    wire logic w1, w2;
+    mux2to1 m1(.x(right), .y(left), .s(LoadLeft), .out(w1));
+    mux2to1 m2(.x(D), .y(w1), .s(loadn), .out(w2));
+    always_ff @(posedge clock)
+    begin
+        if(reset) begin
+            Q <= 0;
+        end
+        else
+        begin
+            Q <= w2;
+        end
+    end
+endmodule
+module mux2to1(x, y, s, out);
+    input logic x, y, s;
+    output logic out;
+    assign out = s ? y : x;
 endmodule
