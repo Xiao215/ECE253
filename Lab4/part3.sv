0bcdb0cf572c763f23b4feaf315ea97ec29227d3
Xiao215 <xiaozhang20030215@gmail.com>
2022-10-26 17:17:47 -0400

    updated and fixed

--- Lab4/part3.sv
+++ Lab4/part3.sv
@@ -2,12 +2,14 @@ module part3(clock, reset, ParallelLoadn, RotateRight, ASRight, Data_IN, Q);
     input logic [3:0] Data_IN;
     input logic clock, reset, ParallelLoadn, RotateRight, ASRight;
     output logic [3:0] Q;
+    assign Q=Data_IN;
     logic [3:0] result;
-    assign result = Data_IN;
-    part3b p1(.left(Data_IN[0]), .right(Data_IN[2]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[3]), .Q(Q[3]), .ASRight(ASRight));
-    part3a p2(.left(Data_IN[3]), .right(Data_IN[1]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[2]),  .Q(Q[2]));
-    part3a p3(.left(Data_IN[2]), .right(Data_IN[0]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[1]), .Q(Q[1]));
-    part3a p4(.left(Data_IN[1]), .right(Data_IN[3]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(Data_IN[0]), .Q(Q[0]));
+    assign result= Q;
+    part3a p1(.left(result[0]), .right(result[2]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(result[3]), .Q(Q[3]));
+    part3a p2(.left(result[3]), .right(result[1]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(result[2]),  .Q(Q[2]));
+    part3a p3(.left(result[2]), .right(result[0]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(result[1]), .Q(Q[1]));
+    part3a p4(.left(result[1]), .right(result[3]), .clock(clock), .reset(reset), .loadn(ParallelLoadn), .LoadLeft(RotateRight), .D(result[0]), .Q(Q[0]));
+
 endmodule
 
 
@@ -21,12 +23,10 @@ module part3a(left, right, LoadLeft,D, loadn, clock, reset, Q);
     begin
         if(reset) begin
             Q <= 0;
-            D<=0;
         end
         else
         begin
             Q <= w2;
-            D<=w2;
         end
     end
 endmodule
@@ -36,18 +36,15 @@ module part3b(left, right, LoadLeft,D, loadn, clock, reset, Q, ASRight);
     wire logic w1, w2, w3;
     mux2to1 m1(.x(right), .y(left), .s(LoadLeft), .out(w1));
     mux2to1 m2(.x(D), .y(w1), .s(loadn), .out(w2));
-    mux2to1 m3(.x(w2), .y(D), .s(ASRight), .out(w3));
     always_ff @(posedge clock)
     begin
         if(reset)
         begin
             Q <= 0;
-            D<=0;
         end
         else
         begin
             Q <= w3;
-            D<=w3;
         end
     end
 endmodule
